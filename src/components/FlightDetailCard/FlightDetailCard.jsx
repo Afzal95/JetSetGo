36ed7238cb8d248b8a2b56cfaf83f50938cdfe1f
Afzal Ahmad <afzal@Afzals-MacBook-Pro.local>
2024-03-25 11:08:15 +0530

    modified time format

--- src/components/FlightDetailCard/FlightDetailCard.jsx
+++ src/components/FlightDetailCard/FlightDetailCard.jsx
@@ -2,14 +2,21 @@ import React from 'react'
 import Card from './Card';
 
 const FlightDetailCard = ({detail}) => {
+  const time = (date)=>{
+    let hr = new Date(date).getHours();
+    let min = new Date(date).getMinutes();
+    hr= hr<10?'0'+hr:hr;
+    min= min<10?'0'+min:min;
+    return `${hr}:${min}`;
+  }
 
   return (
         <div key={detail.id} className="flex justify-center bg-slate-200">
         <Card firstValue={detail.airline} secondValue={detail.flightNumber}  />
-        <Card firstValue={detail.departureTime} secondValue={detail.origin}  />
+        <Card firstValue={time(detail.departureTime)} secondValue={detail.origin}  />
         <Card firstValue={detail.duration} />
-        <Card firstValue={detail.arrivalTime} secondValue={detail.destination}  />
-        <Card firstValue={detail.price} />
+        <Card firstValue={time(detail.arrivalTime)} secondValue={detail.destination}  />
+        <Card firstValue={'Rs '+detail.price} />
         </div>
   )
 }
